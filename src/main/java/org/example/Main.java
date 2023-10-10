6abb90eeb3358518c5789bb82269b9d5fe3f11fd
Dmitrii Bedenchuk <xamwork21@gmail.com>
2023-10-10 19:29:46 +0400

    3 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,12 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello ");
         System.out.println("Hello world!");
+        System.out.println("Hello worASDFASDFAFDASDld!");
+        System.out.println("Hello woSFSDFASDFASDFrld!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
     }
 }
