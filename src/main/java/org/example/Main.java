944a3497698297f2c543f92f69d903ee23afe8bf
Dmitrii Bedenchuk <xamwork21@gmail.com>
2023-10-11 14:36:34 +0400

    bug_fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello ");
-        System.out.println("Hello world!");
-        System.out.println("Hello ");
-        System.out.println("Hello world!");
-        System.out.println("Hello worASDFASDFAFDASDld!");
-        System.out.println("Hello woSFSDFASDFASDFrld!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
 
     }
 }
